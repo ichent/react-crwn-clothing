1c8ab90bf78aeccbd6588e11590974eb32e606bc
Ivan Kishkevich <inkishkevich@tsum.ru>
2020-05-28 23:15:30 +0300

    a sign up component

--- src/pages/sing-in-and-sign-up/sing-in-and-sign-up-component.jsx
+++ src/pages/sing-in-and-sign-up/sing-in-and-sign-up-component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './sing-in-and-sign-up-component.scss';
 import SignInComponent from "../../components/sign-in/sign-in-component";
+import SignUpComponent from "../../components/sign-up/sign-up-component";
 
 const SingInAndSignUpPage = () => (
     <div className='sing-in-and-sign-up'>
         <SignInComponent/>
+        <SignUpComponent/>
     </div>
 )
 
